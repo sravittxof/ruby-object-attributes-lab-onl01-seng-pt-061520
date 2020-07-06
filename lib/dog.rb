a6bbf5612cb69312ab973417254c4546f1693c50
Travis Fox <travis.fox90@gmail.com>
2020-07-06 23:59:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,5 +3,16 @@ class Dog
     @this_dog_name = dog_name
   end
   
+  def name
+    @this_dog_name
+  end
+  
+  def breed=(dog_breed)
+    @this_dog_breed = dog_breed
+  end
+  
+  def breed
+    @this_dog_breed
+  end
   
 end
