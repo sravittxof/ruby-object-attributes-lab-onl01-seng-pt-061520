f28e266e4b078e79ec87ae18489fc8f1060a177f
Travis Fox <travis.fox90@gmail.com>
2020-07-07 00:00:22 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name=(person_name)
+    @this_person_name = person_name
+  end
+  
+  def name
+    @this_person_name
+  end
+  
+  def job=(person_job)
+    @this_person_job = person_job
+  end
+  
+  def job
+    @this_person_job
+  end
+  
+end
